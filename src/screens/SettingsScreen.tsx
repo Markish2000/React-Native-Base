c8c1344eba605b8ee370e2afdd92e9f34fa04513
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-05-05 01:24:43 -0300

    context

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { styles } from '../theme/appTheme';
+import {useContext} from 'react';
+import {Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {styles} from '../theme/appTheme';
+import {AuthContext} from '../context/AuthContext';
 
 export const SettingsScreen = () => {
+  const insets = useSafeAreaInsets();
 
-    const insets = useSafeAreaInsets();
+  const {authState} = useContext(AuthContext);
 
-    return (
-        <View style={{ 
-            ...styles.globalMargin,
-            marginTop: insets.top + 20
-        }}>
-            <Text style={ styles.title }>Settings Screen</Text>
-        </View>
-    )
-}
+  return (
+    <View
+      style={{
+        ...styles.globalMargin,
+        marginTop: insets.top + 20,
+      }}>
+      <Text style={styles.title}>Settings Screen</Text>
+      <Text>{JSON.stringify(authState, null, 4)}</Text>
+    </View>
+  );
+};
